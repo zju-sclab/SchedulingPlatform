d38cc97c5e4f3ff42b4faf70bf9a8e4d088a4309
Lin <921125linxu>
2019-05-17 11:59:13 +0800

    添加gitignore忽略日志文件

---
+++ web/src/main/java/com/skywilling/cn/web/service/impl/LockProducer.java
@@ -0,0 +1,49 @@
+package com.skywilling.cn.web.service.impl;
+
+import com.skywilling.cn.common.kafka.KafkaProperties;
+import org.apache.kafka.clients.producer.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+public class LockProducer {
+
+  private static final Logger LOG = LoggerFactory.getLogger(LockProducer.class);
+  private static final String LOCK_TOPIC = "LOCK";
+  private static final String LOCK_OPERATION = "lock";
+  private static final String UNLOCK_OPERATION = "unlock";
+
+  private Producer<String, String> producer;
+
+  private KafkaProperties kafkaProperties;
+
+  LockProducer(KafkaProperties kafkaProperties) {
+    this.kafkaProperties = kafkaProperties;
+  }
+
+  void init() {
+    producer = new KafkaProducer<>(kafkaProperties.getProducerProperties());
+  }
+
+  /**
+   * write Lock Msg
+   */
+  public void writeLockMsg(String vin) {
+    producer.send(new ProducerRecord<>(LOCK_TOPIC, vin, LOCK_OPERATION),
+            (metadata, exception) -> LOG.info("writeLockMsg success", "write topic: %s, offset: %s", metadata.topic(),
+                metadata.offset()));
+  }
+
+  /**
+   * write Unlock Operation
+   */
+  public void writeUnlockMsg(String vin) {
+    producer.send(new ProducerRecord<>(LOCK_TOPIC, vin, UNLOCK_OPERATION));
+  }
+
+  void destroy() {
+    if (producer != null) {
+      producer.close();
+    }
+  }
+}
